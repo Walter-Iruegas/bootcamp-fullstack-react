68983f9af56b9c090b3d70084840c8c1b1c6df3b
WalterIruegas <walter.iruegas@outlook.com>
2023-04-19 16:20:31 -0500

    commit branch class 2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 //Importaciones a recursos externos
+//Los recursos externo son los que se intalan e n intalacionde una dependencia
 
 //Importaciones de componentes internos
-import Card from "./components/Card"
-import Imagenes from "./components/Imagenes"
-import Component from "./components/Component";
+import Clase2  from "./clase2/Clase2";
+
 //Importar dependencias
 //importar archivo de assets y hojas de estilos
-import logo from "./logo.svg"
 //import './App.css';
 
-function App() {
-  //Logica del componente
-  let a = 4;
-  let b = 8;
-  let result = a + b;
-  let varHola = "Hola mundo";
+const App = () => { 
+   //Logica del componente
 
 
   //Renderizar componentes --  dentro de return 
   //para agregar una variable dentro del renderizado o en una etiqueta usar corchetes {variable}
   return (
-    <div className="App">
-    <img src={logo} alt="logo"></img>
-      {result}
-      <p>{varHola}</p>
-      <pre> {6 * 6} </pre>
-      <Card/>
-      <Imagenes/>
-      <Component></Component>
+    <div>
+      <Clase2/>
     </div>
 
 
